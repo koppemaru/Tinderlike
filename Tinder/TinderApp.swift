39e069351f3abf878a8676754eb346682cb4455d
koppemaru <kohei.maruyama.t8@dc.tohoku.ac.jp>
2024-01-18 21:58:27 +0900

    Initial Commit

---
+++ Tinder/TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderApp.swift
+//  Tinder
+//
+//  Created by Kohei Maruyama on 2024/01/18.
+//
+
+import SwiftUI
+
+@main
+struct TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
